d44adb8791ccfd0e166dddbf0d0c62c2292d7d27
Viet Pham <phamhongviet52@gmail.com>
2016-03-09 15:25:43 +0700

    implement enqueue

--- resque.go
+++ resque.go
@@ -5,11 +5,25 @@ import (
 )
 
 type resque struct {
-	client    redis.Client
-	namespace string
-	queue     string
+	client    *redis.Client
+	Redis     string
+	Namespace string
+	Queue     string
 }
 
-func (*resque) Enqueue(j *job) error {
-	return nil
+func (res *resque) Dial() (err error) {
+	res.client, err = redis.Dial("tcp", res.Redis)
+	return err
+}
+
+func (res *resque) Enqueue(j job) error {
+	queue := res.Namespace + ":queue:" + res.Queue
+
+	jobString, err := j.String()
+	if err != nil {
+		return err
+	}
+
+	reply := res.client.Cmd("RPUSH", queue, jobString)
+	return reply.Err
 }
--- resque_test.go
+++ resque_test.go
@@ -1,8 +1,36 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestResqueEnqueue(test *testing.T) {
+	j := &jobV1{
+		Class: "Test",
+		Args: []string{
+			"a=1",
+			"b=2",
+			"c=3",
+		},
+	}
+
+	resque := resque{
+		Redis:     redisTestServerAddress(),
+		Namespace: "resque",
+		Queue:     "test",
+	}
+
+	err := resque.Dial()
+	if err != nil {
+		test.Errorf("Failed to connect to redis: %s", err.Error())
+	}
+	err = resque.Enqueue(j)
+	if err != nil {
+		test.Errorf("Failed to enqueue: %s", err.Error())
+	}
+}
+
+func redisTestServerAddress() string {
+	return os.Getenv("REDIS_PORT_6379_TCP_ADDR") + ":" + os.Getenv("REDIS_PORT_6379_TCP_PORT")
 }
