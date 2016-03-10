209ade8a0ba3b1c1ba3a5f307bf6fb8637623e6e
Viet Pham <phamhongviet52@gmail.com>
2016-03-11 01:25:31 +0700

    ignore binary file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	defaultRedisAddress   = "127.0.0.1:6379"
+	defaultRedisNameSpace = "resque"
+	defaultQueue          = "tadis"
+	defaultClass          = "tadis"
+	defaultURL            = "/api"
+	defaultPort           = "7415"
+)
+
+func main() {
+	resque := &resque{
+		Redis:     defaultRedisAddress,
+		Namespace: defaultRedisNameSpace,
+		Queue:     defaultQueue,
+	}
+
+	http.HandleFunc(defaultURL, func(writer http.ResponseWriter, request *http.Request) {
+		handler(writer, request, resque, defaultClass)
+	})
+	err := http.ListenAndServe(":"+defaultPort, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
