b9e770f4c92440a2ce8a846275f56c134f3aaca4
Viet Pham <phamhongviet52@gmail.com>
2016-03-10 23:29:28 +0700

    handle request

--- handler.go
+++ handler.go
@@ -2,7 +2,20 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request, queue enqueuer, class string) {
+	data, err := parseRequest(request)
+	if err != nil {
+		fmt.Printf("Failed to parse request: %s", err.Error())
+		return
+	}
+
+	job := newJobV1(class, data)
+
+	err = queue.Enqueue(&job)
+	if err != nil {
+		fmt.Printf("Failed to enqueue job: %s", err.Error())
+	}
 }
